dd77349901564ac662e488638ad8e930c8a1ec29
hyeminleeee <wds5329@naver.com>
2025-08-05 10:38:11 +0900

    배포 test8

--- src/main/java/com/next/app/api/user/controller/HelloController.java
+++ src/main/java/com/next/app/api/user/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@Tag(name = "Hello Controller", description = "간단한 테스트 API")
+@Tag(name = "Hello Controller", description = "간단한 혜민 테스트 API")
 public class HelloController {
 
     @GetMapping("/hello")
